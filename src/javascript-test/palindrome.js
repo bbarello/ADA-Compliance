bfabab5da22fed30fc39c2eed78243f0f4037600
Bryan Barello <bryanbarello@gmail.com>
2021-07-06 18:36:03 -0700

    created palindrome.js file with export

--- src/javascript-test/palindrome.js
+++ src/javascript-test/palindrome.js
@@ -1,1 +1,27 @@
-/* Export your NodeJS module from this file */
+/* npm install express from this file */
+
+// Declares var isPalindrome as function 
+var isPalindrome = function(str){
+    var reversedString = str
+    .split("")
+    .reverse("")
+    .joint("");
+
+// Checks if string is empty, if empty returns false
+if(isPalindrome === "") {
+    //console.log("False");
+    return false;
+}
+// Checks if string is palindrone and returns true
+if (reversedString === str){
+    return true;
+
+// Else if string is not palindrone returns false
+} else {
+    return false;
+}
+
+};
+
+// Exports Module
+module.exports = isPalindrome;
